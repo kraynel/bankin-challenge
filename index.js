35b1e89154ade4c0239a63adb2d97377977819cf
kraynel <kraynel@yahoo.fr>
2018-01-14 11:00:13 +0100

    Save transactions to file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 const scraper = require("./src/scraper");
 
 (async () => {
   const browser = await puppeteer.launch();
   const transactions = await scraper(browser);
-
-  console.log(transactions);
   await browser.close();
+
+  fs.writeFile("transactions.json", JSON.stringify(transactions), function(
+    err
+  ) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("transactions.json saved");
+  });
 })();
